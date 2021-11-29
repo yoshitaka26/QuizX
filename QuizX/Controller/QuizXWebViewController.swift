f49299c32e50532bd993a8466ded6487470d7eea
yoshitaka <yoshitaka.tanaka.1989@gmail.com>
2021-11-29 13:03:03 +0900

    fix : webView shows on navigationBar with certain constraints

--- QuizX/Controller/QuizXWebViewController.swift
+++ QuizX/Controller/QuizXWebViewController.swift
@@ -11,29 +11,42 @@ import WebKit
 
 class QuizXWebViewController: UIViewController {
     
+    @IBOutlet weak var webViewContainer: UIView!
+    
     var webView: WKWebView!
     
-    override func loadView() {
-        
+    required init?(coder: NSCoder) {
         // 2 WKWebViewConfiguration の生成
-        let webConfiguration = WKWebViewConfiguration()
+        let configuration = WKWebViewConfiguration()
+        
+        configuration.applicationNameForUserAgent = "Version/1.0 ShinyBrowser/1.0"
         // 3 WKWebView に Configuration を引き渡し initialize
-        webView = WKWebView(frame: .zero, configuration: webConfiguration)
-        // 4 WKUIDelegate の移譲先として self を登録
-        webView.uiDelegate = self
-        // 5 WKNavigationDelegate の移譲先として self を登録
-        webView.navigationDelegate = self
-        // 6 view に webView を割り当て
-        view = webView
+        webView = WKWebView(frame: .zero, configuration: configuration)
+
+        super.init(coder: coder)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // 4 WKUIDelegate の移譲先として self を登録
+        webView.uiDelegate = self
+        // 5 WKNavigationDelegate の移譲先として self を登録
+        webView.navigationDelegate = self
+        
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        
         // 7 URLオブジェクトを生成
         let myURL = URL(string:"https://quizx.net")
         // 8 URLRequestオブジェクトを生成
         let myRequest = URLRequest(url: myURL!)
+        
+        webViewContainer.addSubview(webView)
+        
+        NSLayoutConstraint.activate([
+            webView.widthAnchor.constraint(equalTo: webViewContainer.widthAnchor),
+            webView.heightAnchor.constraint(equalTo: webViewContainer.heightAnchor)
+        ])
         
         // 9 URLを WebView にロード
         webView.load(myRequest)
